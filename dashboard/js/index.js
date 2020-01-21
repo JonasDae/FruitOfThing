853bc7dda428b4bd8aae73a7b10fdfdb6c61dadb
culacant <r0463164@student.thomasmore.be>
2020-01-21 11:28:16 +0100

    charts gefok

--- dashboard/js/index.js
+++ dashboard/js/index.js
@@ -1,34 +1,51 @@
-console.log("HW");
-
+// graph
+// datasets
+var datasets = [[12, 19, 3, 5, 2, 3],
+				[11, 18, 5, 2, 12, 13],
+				[1, 8, 15, 8, 5, 8]];
+// checkboxes
+$('input[class=chk_dataset]').change(function() {
+	var value = $(this).val();
+	if($(this).is(':checked')) {
+		chart_out.data.datasets[value].data = datasets[value];
+		chart_out.update();
+	}
+	else {
+		chart_out.data.datasets[value].data = [];
+		chart_out.update();
+	}
+});
+// canvas
 var cnv_graph = document.getElementById("cnv_graph").getContext("2d");
 
+// init chart
 var chart_out = new Chart(cnv_graph, {
     type: 'bar',
     data: {
         labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
         datasets: [{
-            label: '# of Votes',
-            data: [12, 19, 3, 5, 2, 3],
-            backgroundColor: [
-                'rgba(255, 99, 132, 0.2)',
-                'rgba(54, 162, 235, 0.2)',
-                'rgba(255, 206, 86, 0.2)',
-                'rgba(75, 192, 192, 0.2)',
-                'rgba(153, 102, 255, 0.2)',
-                'rgba(255, 159, 64, 0.2)'
-            ],
-            borderColor: [
-                'rgba(255, 99, 132, 1)',
-                'rgba(54, 162, 235, 1)',
-                'rgba(255, 206, 86, 1)',
-                'rgba(75, 192, 192, 1)',
-                'rgba(153, 102, 255, 1)',
-                'rgba(255, 159, 64, 1)'
-            ],
-            borderWidth: 1
-        }]
+            label: 'set 0',
+            data: [],
+            borderWidth: 1,
+			backgroundColor: '#ff6574',
+        },
+		{
+            label: 'set 1',
+            data: [],
+            borderWidth: 1,
+			backgroundColor: '#74ff65',
+		},
+		{
+            label: 'set 2',
+            data: [],
+            borderWidth: 1,
+			backgroundColor: '#6574ff',
+		}]
     },
     options: {
+		legend: {
+			display: false
+		},
         scales: {
             yAxes: [{
                 ticks: {
