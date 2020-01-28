370df45dd6a505b1b91138d5c8e6747e7eb9c18e
Tjitsen Vermeulen <r0709243@student.thomasmore.be>
2020-01-28 15:37:58 +0100

    replaced links by popovers

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -98,6 +98,19 @@
 </footer>
 
 <script src="{{ asset('js/index.js') }}" defer></script>
+<script>
+    //enable popovers in the modules page
+    $(function () {
+        $('[data-toggle="popover"]').popover();
+
+        //close popover on click outside
+        $('body').on('click', function (e) {
+            if ($(e.target).data('toggle') !== 'popover' && $(e.target).parents('.popover.in').length === 0) {
+                $('[data-toggle="popover"]').popover('hide');
+            }
+        });
+    })
+</script>
 
 </body>
 </html>
