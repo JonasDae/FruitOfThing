1d93c33fd953aadb0bc85891d0208a744d886a9a
Tjitsen Vermeulen <r0709243@student.thomasmore.be>
2020-01-29 09:13:08 +0100

    profile form updated validation messages

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -8,7 +8,10 @@
                 <i class="fas fa-user-circle h-75 w-75"></i>
             </div>
             <div class="col-lg-8 col-md-6 col-12">
-                <form action="" class="p-3">
+                <form action="{{ route('profile.update') }}" class="p-3">
+
+                    <input type="hidden" name="token" value="{{ $token }}">
+
                     <div class="form-group row">
                         <label for="name" class="col-md-4 col-form-label">Naam</label>
                         <div class="col-md-6">
@@ -18,13 +21,23 @@
                     <div class="form-group row">
                         <label for="email" class="col-md-4 col-form-label">E-Mail</label>
                         <div class="col-md-6">
-                            <input id="email" type="email" class="form-control" name="email" value="{{ $profile->email }}" required autocomplete="email">
+                            <input id="email" type="email" class="form-control" name="email @error('email') is-invalid @enderror" value="{{ $profile->email }}" required autocomplete="email">
+                            @error('email')
+                            <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                            @enderror
                         </div>
                     </div>
                     <div class="form-group row">
                         <label for="password" class="col-md-4 col-form-label">Wachtwoord</label>
                         <div class="col-md-6">
-                            <input id="password" type="password" class="form-control " name="password" autocomplete="new-password">
+                            <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" autocomplete="new-password">
+                            @error('password')
+                            <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                            @enderror
                         </div>
                     </div>
                     <div class="form-group row">
