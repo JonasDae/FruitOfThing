7b67f5f841375cbedd7a4797410f1abbd5a28495
Tjitsen Vermeulen <r0709243@student.thomasmore.be>
2020-01-29 12:36:03 +0100

    changed profile page

--- resources/views/profiles/index.blade.php
+++ resources/views/profiles/index.blade.php
@@ -14,35 +14,13 @@
                     <div class="form-group row">
                         <label for="name" class="col-md-4 col-form-label">Naam</label>
                         <div class="col-md-6">
-                            <input id="name" type="text" class="form-control" name="name" value="{{ old('name') || $profile->name }}" required autocomplete="name" autofocus>
+                            <input id="name" type="text" class="form-control" name="name" value="{{ old('name') ?? $profile->name }}" autocomplete="name" autofocus>
                         </div>
                     </div>
                     <div class="form-group row">
                         <label for="email" class="col-md-4 col-form-label">E-Mail</label>
                         <div class="col-md-6">
-                            <input id="email" type="email" class="form-control" name="email @error('email') is-invalid @enderror" value="{{ old('email') || $profile->email }}" required autocomplete="email">
-                            @error('email')
-                            <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                            @enderror
-                        </div>
-                    </div>
-                    <div class="form-group row">
-                        <label for="password" class="col-md-4 col-form-label">Wachtwoord</label>
-                        <div class="col-md-6">
-                            <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" autocomplete="new-password">
-                            @error('password')
-                            <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                            @enderror
-                        </div>
-                    </div>
-                    <div class="form-group row">
-                        <label for="password-confirm" class="col-md-4 col-form-label">Wachtwoord Bevestigen</label>
-                        <div class="col-md-6">
-                            <input id="password-confirm" type="password" class="form-control" name="password_confirmation" autocomplete="new-password">
+                            <input id="email" type="email" class="form-control" name="email" value="{{ $profile->email }}" disabled>
                         </div>
                     </div>
                     <div class="form-group row mb-0">
