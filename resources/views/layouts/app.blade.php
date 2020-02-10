e9e7e27911e06235b56938d16fb750b59bc86019
Tjitsen Vermeulen <r0709243@student.thomasmore.be>
2020-02-10 13:37:36 +0100

    added google maps on field page

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -17,85 +17,86 @@
     <link href="{{ asset('css/index.css') }}" rel="stylesheet">
 </head>
 <body>
-<div id='app'></div>{{--VUE error fix--}}
-<header>
-    <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
-        <a class="navbar-brand" href="https://www.pcfruit.be/nl">
-            <img src="{{ asset('img/logo.png') }}" height="50" alt="logo">
-        </a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
-                aria-controls="navbarSupportedContent" aria-expanded="false"
-                aria-label="{{ __('Toggle navigation') }}">
-            <span class="navbar-toggler-icon"></span>
-        </button>
+<div id="app" class="d-flex">
+    <header>
+        <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
+            <a class="navbar-brand" href="https://www.pcfruit.be/nl">
+                <img src="{{ asset('img/logo.png') }}" height="50" alt="logo">
+            </a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
+                    aria-controls="navbarSupportedContent" aria-expanded="false"
+                    aria-label="{{ __('Toggle navigation') }}">
+                <span class="navbar-toggler-icon"></span>
+            </button>
 
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <!-- Left Side Of Navbar -->
-            <ul class="navbar-nav mr-auto">
-                <li class="nav-item">
-                    <a href="{{ route('home') }}" class="nav-link{{ Request::path() == '/' ? ' active' : '' }}">{{ __('Home') }}</a>
-                </li>
-                @auth
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <!-- Left Side Of Navbar -->
+                <ul class="navbar-nav mr-auto">
                     <li class="nav-item">
-                        <a href="{{ route('fruits.index') }}" class="nav-link{{ Request::path() == 'fruit_types' ? ' active' : '' }}">{{ __('Fruitsoorten') }}</a>
+                        <a href="{{ route('home') }}" class="nav-link{{ Request::path() == '/' ? ' active' : '' }}">{{ __('Home') }}</a>
                     </li>
-                    <li class="nav-item">
-                        <a href="{{ route('fields.index') }}" class="nav-link{{ Request::path() == 'fields' ? ' active' : '' }}">{{ __('Velden') }}</a>
-                    </li>
-                    <li class="nav-item">
-                        <a href="{{ route('modules.index') }}" class="nav-link{{ Request::path() == 'modules' ? ' active' : '' }}">{{ __('Modules') }}</a>
-                    </li>
-                    <li class="nav-item">
-                        <a href="{{ route('sensors.index') }}" class="nav-link{{ Request::path() == 'sensors' ? ' active' : '' }}">{{ __('Sensoren') }}</a>
-                    </li>
-                @endauth
-            </ul>
-
-            <!-- Right Side Of Navbar -->
-            <ul class="navbar-nav ml-auto">
-                <!-- Authentication Links -->
-                @guest
-                    <li class="nav-item">
-                        <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
-                    </li>
-                    @if (Route::has('register'))
+                    @auth
                         <li class="nav-item">
-                            <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
+                            <a href="{{ route('fruits.index') }}" class="nav-link{{ Request::path() == 'fruit_types' ? ' active' : '' }}">{{ __('Fruitsoorten') }}</a>
                         </li>
-                    @endif
-                @else
-                    <li class="nav-item dropdown">
-                        <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button"
-                           data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                            {{ Auth::user()->name }} <span class="caret"></span>
-                        </a>
+                        <li class="nav-item">
+                            <a href="{{ route('fields.index') }}" class="nav-link{{ Request::path() == 'fields' ? ' active' : '' }}">{{ __('Velden') }}</a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="{{ route('modules.index') }}" class="nav-link{{ Request::path() == 'modules' ? ' active' : '' }}">{{ __('Modules') }}</a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="{{ route('sensors.index') }}" class="nav-link{{ Request::path() == 'sensors' ? ' active' : '' }}">{{ __('Sensoren') }}</a>
+                        </li>
+                    @endauth
+                </ul>
 
-                        <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                            <a class="dropdown-item" href="{{ route('profile.index') }}">{{ __('Profiel') }}</a>
-                            <a class="dropdown-item" href="{{ route('logout') }}"
-                               onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
-                                {{ __('Logout') }}
+                <!-- Right Side Of Navbar -->
+                <ul class="navbar-nav ml-auto">
+                    <!-- Authentication Links -->
+                    @guest
+                        <li class="nav-item">
+                            <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
+                        </li>
+                        @if (Route::has('register'))
+                            <li class="nav-item">
+                                <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
+                            </li>
+                        @endif
+                    @else
+                        <li class="nav-item dropdown">
+                            <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button"
+                               data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                                {{ Auth::user()->name }} <span class="caret"></span>
                             </a>
-                            <form id="logout-form" action="{{ route('logout') }}" method="POST"
-                                  style="display: none;">
-                                @csrf
-                            </form>
-                        </div>
-                    </li>
-                @endguest
-            </ul>
-        </div>
-    </nav>
-</header>
 
-<div id="content">
-    @yield('content')
-</div>
+                            <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                                <a class="dropdown-item" href="{{ route('profile.index') }}">{{ __('Profiel') }}</a>
+                                <a class="dropdown-item" href="{{ route('logout') }}"
+                                   onclick="event.preventDefault();
+                                                     document.getElementById('logout-form').submit();">
+                                    {{ __('Logout') }}
+                                </a>
+                                <form id="logout-form" action="{{ route('logout') }}" method="POST"
+                                      style="display: none;">
+                                    @csrf
+                                </form>
+                            </div>
+                        </li>
+                    @endguest
+                </ul>
+            </div>
+        </nav>
+    </header>
 
-<footer>
-    <p>&copy; Copyright {{ date('Y') }} Fruit Of Things</p>
-</footer>
+    <div id="content">
+        @yield('content')
+    </div>
+
+    <footer>
+        <p>&copy; Copyright {{ date('Y') }} Fruit Of Things</p>
+    </footer>
+</div>{{--VUE error fix--}}
 
 <script src="{{ asset('js/index.js') }}" defer></script>
 
