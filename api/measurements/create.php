8382546bfcb484003603405b9fdf4ea43e75be71
culacant <r0463164@student.thomasmore.be>
2020-02-11 09:36:52 +0100

    fix measurement create

--- api/measurements/create.php
+++ api/measurements/create.php
@@ -3,7 +3,10 @@ require '../connect.php';
 
 $postdata = file_get_contents("php://input");
 
+// OLD
 // $postdata = '{"module_id": 1, "battery_level": 34, "module_sensor_id": 3, "value": 20, "measure_date": "2020-01-30 10:20:20"}';
+// NEW
+//$postdata = '{"module_id": 1, "battery_level": 34, "measure_date": "2020-01-30 10:20:20", data: [{sensor: 1, data: 20.3},{sensor: 2, data: 30.5},{sensor: 3, data: 40.6},{sensor: 4, data: 50.1}]}';
 
 if(isset($postdata) && !empty($postdata))
 {    
@@ -15,19 +18,25 @@ if(isset($postdata) && !empty($postdata))
     // Saniteze json
     $module_id = intval(mysqli_real_escape_string($con, $request->module_id)); //int
     $battery_level = intval(mysqli_real_escape_string($con, $request->battery_level)); //int
-    $module_sensor_id = intval(mysqli_real_escape_string($con, $request->module_sensor_id)); //int
-    $value = doubleval(mysqli_real_escape_string($con, $request->value)); //double
     $measure_date = DateTime::createFromFormat('Y-m-d H:i:s', mysqli_real_escape_string($con, $request->measure_date))->format('Y-m-d H:i:s');
     // date("Y-m-d H:i:s", strtotime(mysqli_real_escape_string($con, $request->measure_date))); //dateTime   
 
+    // Store measurements
+	foreach($request->data as $data)
+	{
+	/*
+    $module_sensor_id = intval(mysqli_real_escape_string($con, $request->module_sensor_id)); //int
+    $value = doubleval(mysqli_real_escape_string($con, $request->value)); //double
+	*/
+		echo "data element:" . $data;
+		$sql = "INSERT INTO measurements (module_id, module_sensor_id, value, measure_date)
+				VALUES (
+				'{$module_id}',
+				'{$data->sensor}',
+				'{$data->value}',
+				'{$measure_date}')";
+	}
 
-    // Store measurement
-    $sql = "INSERT INTO measurements (module_id, module_sensor_id, value, measure_date)
-            VALUES (
-            '{$module_id}',
-            '{$module_sensor_id}',
-            '{$value}',
-            '{$measure_date}')";
 
     if(sql_query($con, $sql)) // Store succes
     {
