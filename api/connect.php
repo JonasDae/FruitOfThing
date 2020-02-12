55f2f1a794aae9803d8a71aceb78d9be2285ee5e
Florian D'Haene <dhaene_florian@hotmail.com>
2020-02-12 16:41:17 +0100

    api shizzle

--- api/connect.php
+++ api/connect.php
@@ -9,16 +9,22 @@ define('DB_PASS', '');
 define('DB_NAME', 'pcfruit');
 
 // db credentials SINNERS
-// define('DB_HOST', 'localhost');
-// define('DB_USER', 'root');
+// define('DB_HOST', 'db.sinners.be');
+// define('DB_USER', 'floriandh');
 // define('DB_PASS', '');
-// define('DB_NAME', 'pcfruit');
+// define('DB_NAME', 'floriandh_pcfruit');
 
 // db credentials INFINITY
 // define('DB_HOST', 'sql107.epizy.com');
 // define('DB_USER', 'epiz_25192099');
 // define('DB_PASS', '9uWkPRY99pWL1');
 // define('DB_NAME', 'epiz_25192099_pcfruit_dashboard');
+
+// db credentials INFINITY
+// define('DB_HOST', 'dekreekvzw.be.mysql');
+// define('DB_USER', 'dekreekvzw_bepcfruit_db');
+// define('DB_PASS', 'FruitOfThings');
+// define('DB_NAME', 'dekreekvzw_bepcfruit_db');
 
 // connect with the database
 function sql_connect() {
