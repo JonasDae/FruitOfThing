d8caa97919ee9d88ed49173f121943598cfad065
Tjitsen Vermeulen <r0709243@student.thomasmore.be>
2020-02-12 12:44:43 +0100

    improved navbar layout

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -64,7 +64,7 @@
                             </li>
                         @endif
                     @else
-                        <!--Notifications-->
+                    <!--Notifications-->
                         <li id="notifications" class="nav-item dropdown">
                             <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
                                 Meldingen <span class="badge badge-danger">{{ count(auth()->user()->unreadNotifications) }}</span>
@@ -99,21 +99,22 @@
                                 @endif
                             </div>
                         </li>
-                            <!--Profile-->
+                        <!--Profile-->
                         <li class="nav-item dropdown">
                             <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button"
                                data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
                                 {{ Auth::user()->name }} <span class="caret"></span>
                             </a>
 
-                            <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                                <a class="dropdown-item" href="{{ route('profile.index') }}">{{ __('Profiel') }}</a>
-                                <a class="dropdown-item" href="{{ route('logout') }}"
+                            <div class="dropdown-menu dropdown-menu-right py-0" aria-labelledby="navbarDropdown">
+                                <a class="dropdown-item py-3" href="{{ route('profile.index') }}">{{ __('Profiel') }}</a>
+                                <hr class="my-0">
+                                <a class="dropdown-item py-3" href="{{ route('logout') }}"
                                    onclick="event.preventDefault();
                                                      document.getElementById('logout-form').submit();">
                                     {{ __('Logout') }}
                                 </a>
-                                <form id="logout-form" action="{{ route('logout') }}" method="POST"
+                                <form id="logout-form py-3" action="{{ route('logout') }}" method="POST"
                                       style="display: none;">
                                     @csrf
                                 </form>
